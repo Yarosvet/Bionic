f8ae8e8afe6f3a163ae0feb24895bfe9bec5cd1f
Yarosvet <yarosvetk@gmail.com>
2022-08-14 16:33:28 +0300

    Add_dialog made

--- gui_control.py
+++ gui_control.py
@@ -1,5 +1,13 @@
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QDialog, QFileDialog, QErrorMessage
+from PyQt5.QtGui import QCursor
+from PyQt5.QtCore import Qt, QStringListModel, QModelIndex
 from main_window import Ui_MainWindow
+from add_dialog import Ui_AddDialog
+import requests
+import json
+from tarfile import TarFile
+import os
+from shutil import rmtree
 
 
 class MainWin(QMainWindow):
@@ -7,3 +15,76 @@ class MainWin(QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.addDialog = AddDialog()
+        self.ui.add_button.clicked.connect(self.add_clicked)
+
+    def add_clicked(self):
+        self.addDialog.show()
+
+
+def install_book(tar_path, book_id=None):
+    if not os.path.exists("books/"):
+        os.mkdir("books/")
+    if book_id is None:
+        with TarFile.open(name=tar_path, mode="r") as tf:
+            tf.extract("book.json", "temp/")
+        with open("temp/book.json", 'r') as f:
+            book_id = json.load(f)["id"]
+        os.remove("temp/book.json")
+    book_path = f"books/book_{book_id}/"
+    if os.path.exists(book_path):
+        rmtree(book_path)
+    os.mkdir(book_path)
+    with TarFile.open(name=tar_path, mode="r") as tf:
+        tf.extractall(path=book_path)
+
+
+class AddDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.github_rels = None
+        self.error_dialog = QErrorMessage()
+        self.error_dialog.setModal(True)
+        self.ui = Ui_AddDialog()
+        self.ui.setupUi(self)
+        self.ui.select_button.clicked.connect(self.select_clicked)
+        self.ui.get_button.clicked.connect(self.get_clicked)
+        self.ui.listbooks.doubleClicked.connect(self.book_clicked)
+
+    def select_clicked(self):
+        files = QFileDialog.getOpenFileNames(parent=self, caption="Select one or more book-files *.tar.gz",
+                                             filter="Archive GZIP (*.tar.gz);;All files (*.*)")[0]
+        try:
+            for el in files:
+                install_book(el)
+            self.close()
+        except Exception as exc:
+            self.error_dialog.showMessage(str(exc))
+
+    def get_clicked(self):
+        self.ui.listbooks.setCursor(QCursor(Qt.WaitCursor))
+        try:
+            req = requests.get("https://raw.githubusercontent.com/Yarosvet/Bionic_books/master/rels.json")
+            if req.status_code == 200:
+                self.github_rels = json.loads(req.content)
+                self.github_rels["books"] = sorted(self.github_rels["books"], key=lambda x: x["id"])
+                model = QStringListModel([book["name"] for book in self.github_rels["books"]])
+                self.ui.listbooks.setModel(model)
+            else:
+                self.error_dialog.showMessage(f"Request code: {req.status_code}")
+        except Exception as exc:
+            self.error_dialog.showMessage(str(exc))
+        self.ui.listbooks.setCursor(QCursor(Qt.ArrowCursor))
+
+    def book_clicked(self, index_m: QModelIndex):
+        book = self.github_rels["books"][index_m.row()]
+        req = requests.get(self.github_rels["link_prefix"] + book["path"])
+        if req.status_code == 200:
+            with open("book_{}.tar.gz".format(book["id"]), 'wb') as f:
+                f.write(req.content)
+            try:
+                install_book("book_{}.tar.gz".format(book["id"]), book["id"])
+                os.remove("book_{}.tar.gz".format(book["id"]))
+                self.close()
+            except Exception as exc:
+                self.error_dialog.showMessage(str(exc))
